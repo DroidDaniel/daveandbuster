57ffdf32d7115671dddf792b6e5fb810514e617e
Daniel Disowsa <disowsa@gmail.com>
2022-09-27 14:19:25 -0700

    initial commit

---
+++ js/min.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  var dateToday = new Date();
+  var dates = $("#from").datepicker({
+    minDate: dateToday,
+    dateFormat: "M d,y",
+    onSelect: function (selectedDate) {
+      var option = this.id == "from" ? "minDate" : "maxDate",
+        instance = $(this).data("datepicker"),
+        date = $.datepicker.parseDate(
+          instance.settings.dateFormat || $.datepicker._defaults.dateFormat,
+          selectedDate,
+          instance.settings
+        );
+      dates.not(this).datepicker("option", option, date);
+    },
+  });
+});
