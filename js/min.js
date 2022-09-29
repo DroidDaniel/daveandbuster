b44f549b6b88194ca8938a014ed9918af728d529
Daniel Disowsa <disowsa@gmail.com>
2022-09-28 20:01:18 -0700

    initial commit

--- js/min.js
+++ js/min.js
@@ -1,18 +1,23 @@
 $(document).ready(function () {
-  /**********Datepicker***********/
-  var dateToday = new Date();
-  var dates = $("#date_calendar").datepicker({
-    minDate: dateToday,
-    dateFormat: "M d,y",
-    onSelect: function (selectedDate) {
-      var option = this.id == "date_calendar" ? "minDate" : "maxDate",
-        instance = $(this).data("datepicker"),
-        date = $.datepicker.parseDate(
-          instance.settings.dateFormat || $.datepicker._defaults.dateFormat,
-          selectedDate,
-          instance.settings
-        );
-      dates.not(this).datepicker("option", option, date);
-    },
+  $(".gallery-photo-slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: true,
+    dots: false,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 9999,
+        settings: "unslick",
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
 });
