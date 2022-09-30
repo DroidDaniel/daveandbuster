44b746f875b415231e290145b814e734c432a2d8
Daniel Disowsa <disowsa@gmail.com>
2022-09-30 11:44:18 -0700

    initial commit

--- js/min.js
+++ js/min.js
@@ -1,32 +1,3 @@
 $(document).ready(function () {
-  window.onresize = triggerSlideronResize;
-  function slider() {
-    $(".gallery-photo-slider")
-      .not(".slick-initialized")
-      .slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        arrows: true,
-        dots: false,
-        pauseOnHover: true,
-        responsive: [
-          {
-            breakpoint: 9999,
-            settings: "unslick",
-          },
-          {
-            breakpoint: 601,
-            settings: {
-              slidesToShow: 1,
-            },
-          },
-        ],
-      });
-  }
-  function triggerSlideronResize() {
-    slider();
-  }
-  slider();
+  console.log("************");
 });
