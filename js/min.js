8d5bd82b215593d7140d2e6792cc8915557f0752
Daniel Disowsa <disowsa@gmail.com>
2022-10-05 12:04:39 -0700

    play local video

--- js/min.js
+++ js/min.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
-  console.log("************");
+  $(".play_video_btn").click(function () {
+    var showVideoContainer = $(this).next(".video_modal_container");
+    var playVideo = $(this)
+      .next(".video_modal_container")
+      .parent()
+      .find(".video_modal__source")
+      .children();
+    $(showVideoContainer).addClass("show");
+    $(playVideo).trigger("play");
+  });
+
+  $(".close_video_btn").click(function () {
+    var stopVideo = $(".close_video_btn").next();
+    var hideVideoContainer = $(".close_video_btn").parents();
+    $(stopVideo).trigger("pause");
+    $(hideVideoContainer).removeClass("show");
+  });
 });
