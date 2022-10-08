33abdb374594f938703d73cf365b4b706458d93c
Daniel Disowsa <disowsa@gmail.com>
2022-10-08 09:58:47 -0700

    slick dot indicator

--- js/min.js
+++ js/min.js
@@ -1,17 +1,24 @@
 $(document).ready(function () {
   $(".message_slider").slick({
-    dots: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    cssEase: "linear",
+    dots: true,
     infinite: true,
     speed: 300,
     slidesToShow: 1,
     centerMode: true,
     centerPadding: "25%",
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: false,
     responsive: [
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 1,
           centerMode: true,
+          dots: false,
           centerPadding: "15%",
         },
       },
